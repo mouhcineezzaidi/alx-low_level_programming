88272876a4255a65916cef0c2c3ea4a74c56ab66
mouhcineezzaidi <mouhcine1ezzaidi@gmail.com>
2023-03-21 08:01:01 -0700

    Functions and Nested loops in C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - return the absolute value of a given integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: Number used to find the last digit
+ *Description: will print the last digit of a given number. If the
+ * number is a negative the value will be return unsigned int.
+* Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar('0' + n);
+	return (n);
+}
