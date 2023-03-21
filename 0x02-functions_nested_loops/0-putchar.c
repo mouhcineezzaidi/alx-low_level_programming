17603010b87a6d1caed1e439805a103116de840e
mouhcineezzaidi <mouhcine1ezzaidi@gmail.com>
2023-03-21 02:53:01 -0700

    Functions and Nested loops in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+int main(void)
+{
+	char *h = "_putchar\n";
+	while (*h)
+
+	{
+		putchar(*h);
+			h++;
+	}
+	return (0);
+}
