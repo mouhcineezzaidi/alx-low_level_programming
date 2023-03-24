4539e4b100f589d917b0d14a44da0a5e6df57e9c
mouhcineezzaidi <126358536+mouhcineezzaidi@users.noreply.github.com>
2023-03-24 02:20:37 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,17 @@
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
+    int i;
+    
+    for(i = 0; i < 10; i++)
+    {
+        putchar(i + '0');
+        if(i != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    
+    return 0;
 }
