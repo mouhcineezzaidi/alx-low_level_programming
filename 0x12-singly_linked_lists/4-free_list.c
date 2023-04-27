d4892fa136756ea44637d722b054a124184a69f5
mouhcineezzaidi <126358536+mouhcineezzaidi@users.noreply.github.com>
2023-04-27 23:22:35 +0000

    Singly linked lists in C

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *a;
 
 	while (head)
 	{
-		temp = head->next;
+		a = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = a;
 	}
 }
