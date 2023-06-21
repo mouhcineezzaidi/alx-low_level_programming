7332a5be6a296badc90547109fca9ba8aa095682
Mouhcine <mouhcine1ezzaidi@gmail.com>
2023-06-21 19:39:39 +0100

    Dynamic libraries in C

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
+   * main - check the code
+    *
+     * Return: Always EXIT_SUCCESS.
+      */
+int main()
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+	    printf("%d\n", _strlen("My Dyn Lib"));
+	        return (1);
 }
--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -c -Wall -Werror -fpic *.c
-gcc -shared -o liball.so *.o
-
+gcc -fPIC -c *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
